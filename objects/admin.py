c028b28f76109807a3c9caad608ceee0ea24fdb2
Евгений Протозанов <weirdcarrotmonster@gmail.com>
2012-10-23 01:17:02 +0400

    initial commit

---
+++ objects/admin.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from objects.models import *
+
+admin.site.register(Config)
+admin.site.register(ConfigItem)
+admin.site.register(MenuItem)
+admin.site.register(Command)
+admin.site.register(Group)
+admin.site.register(Machine)
---
+++ objects/models.py
@@ -0,0 +1,38 @@
+from django.db import models
+
+# Create your models here.
+class Command(models.Model):
+    def __unicode__(self):
+        return self.name
+    name = models.CharField(max_length=16)
+
+class ConfigItem(models.Model):
+    def __unicode__(self):
+        return self.command.name + " " + self.args
+    command = models.ForeignKey(Command)
+    args = models.CharField(max_length=32)
+
+class MenuItem(models.Model):
+    def __unicode__(self):
+        return self.name
+    name = models.CharField(max_length=32)
+    item = models.ManyToManyField(ConfigItem)
+    
+class Config(models.Model):
+    def __unicode__(self):
+        return self.name
+    name = models.CharField(max_length=32)
+    menuItem = models.ManyToManyField(MenuItem)
+
+class Group(models.Model):
+    def __unicode__(self):
+        return self.name
+    name = models.CharField(max_length=32)
+    config = models.ForeignKey(Config)
+
+class Machine(models.Model):
+    def __unicode__(self):
+        return self.name
+    name = models.CharField(max_length=32)
+    mac = models.CharField(max_length=12)
+    group = models.ForeignKey(Group)
