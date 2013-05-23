1de014486c861c39a6976ede9773cc8771c99046
Евгений Протозанов <weirdcarrotmonster@gmail.com>
2013-05-24 02:25:26 +0400

    PEP8

--- objects/admin.py
+++ objects/admin.py
@@ -2,22 +2,27 @@ from django.contrib import admin
 from objects.models import *
 from django import forms
 
-class ConfigForm( forms.ModelForm ):
-	name = forms.CharField()
-	more = forms.CharField( widget=forms.Textarea )
 
-class ConfigAdmin( admin.ModelAdmin ):
-	form = ConfigForm
+class ConfigForm(forms.ModelForm):
+    name = forms.CharField()
+    more = forms.CharField(widget=forms.Textarea)
+
+
+class ConfigAdmin(admin.ModelAdmin):
+    form = ConfigForm
+
 
 class ConfigItemInline(admin.StackedInline):
-	model = ConfigItem
-	extra = 1
+    model = ConfigItem
+    extra = 1
+
 
 class MenuItemAdmin(admin.ModelAdmin):
-	fieldsets = [
-		(None,               {'fields': ['name']}),
-	]
-	inlines = [ConfigItemInline]
+    fieldsets = [
+        (None, {'fields': ['name']}),
+    ]
+    inlines = [ConfigItemInline]
+
 
 admin.site.register(Config, ConfigAdmin)
 admin.site.register(MenuItem, MenuItemAdmin)
--- objects/models.py
+++ objects/models.py
@@ -1,51 +1,65 @@
 from django.db import models
 from django.conf import settings
-from django.core.exceptions import *
 import os
+
 
 class Command(models.Model):
     def __unicode__(self):
         return self.name
+
     name = models.CharField(max_length=64)
+
 
 class MenuItem(models.Model):
     def __unicode__(self):
         return self.name
+
     name = models.CharField(max_length=64)
+
 
 class ConfigItem(models.Model):
     def __unicode__(self):
         return self.command.name + " " + self.args
+
     command = models.ForeignKey(Command)
     menuItem = models.ForeignKey(MenuItem)
     args = models.CharField(max_length=1024)
 
+
 class Config(models.Model):
     def __unicode__(self):
         return self.name
+
     more = models.CharField(max_length=32768)
     name = models.CharField(max_length=64)
     menuItem = models.ManyToManyField(MenuItem)
 
+
 class Group(models.Model):
     def __unicode__(self):
         return self.name
+
     name = models.CharField(max_length=64)
     config = models.ForeignKey(Config)
+
 
 class Machine(models.Model):
     def __unicode__(self):
         return self.name
+
     def save(self, force_insert=False, force_update=False):
         self.mac = self.mac.lower()
         super(Machine, self).save(force_insert, force_update)
+
     name = models.CharField(max_length=64)
     mac = models.CharField(max_length=17)
     group = models.ForeignKey(Group)
 
+
 class File(models.Model):
     def __unicode__(self):
         return self.name
+
     def save(self, force_insert=False, force_update=False):
         try:
             oldFile = File.objects.get(pk=self.pk)
@@ -54,5 +68,6 @@ class File(models.Model):
             pass
         finally:
             super(File, self).save(force_insert, force_update)
+
     name = models.CharField(max_length=64)
     fileItem = models.FileField(upload_to=settings.MEDIA_ROOT)
